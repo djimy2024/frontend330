c3234526b161e34ae47c38ecbf29b74e045cf660
Francillon2024 <fra24017@byui.edu>
2025-06-07 17:50:11 -0400

    update

--- src/project/auth.js
+++ src/project/auth.js
@@ -5,8 +5,8 @@ export function initAuth() {
   accessToken = params.get('access_token');
 
   document.getElementById('spotify-login').addEventListener('click', () => {
-    const clientId = 'VRE_CLIENT_ID_W'  // Ranplase ak vre Client ID ou
-    const redirectUri = 'http://localhost:5173/';
+    const clientId = '30cf98bb37f04faab1fd4f4fe70ac9c7' 
+    const redirectUri = 'https://djimy2024.github.io/frontend330/src/project/';
     const scopes = 'playlist-modify-public';
     const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${scopes}&redirect_uri=${encodeURIComponent(redirectUri)}`;
     window.location.href = authUrl;
